f82cbe40ffbcbce808bb28ba8625bc011773796f
kazukinakamuraKN <kajirikajiri@gmail.com>
2018-12-04 08:20:18 +0900

    Improve

--- src/framework/Main.java
+++ src/framework/Main.java
@@ -11,12 +11,15 @@ public class Main {
 		manager.register("warning box", mbox);
 		manager.register("slash box", sbox);
 
+		//生成
 		Product p1 = manager.create("strong message");
 		p1.use("Hello,world.");
+		p1.use("google");
 		Product p2 = manager.create("warning box");
 		p2.use("Hello,world.");
+		p2.use("Safari");
 		Product p3 = manager.create("slash box");
 		p3.use("Hello,world.");
-
+		p3.use("");
 	}
 }
